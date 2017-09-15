12bff9cda2d9120d0b223839512bacceabb0860e
codesalad <nguyen094@gmail.com>
2017-09-15 04:53:26 +0200

    update

--- public/ieye/js/ieyewidget.js
+++ public/ieye/js/ieyewidget.js
@@ -250,7 +250,7 @@
 			
 		
 			
-			var tracker = new tracking.ObjectTracker(['face']);	//var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+			var tracker = new tracking.ObjectTracker("face");	//var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
 			tracker.setInitialScale(4);  	// the smaller, the smaller faces it can discover, however small values are in trouble with big faces over all area.
 			tracker.setStepSize(1.7);			// how fast is the recognition
 			tracker.setEdgesDensity(0.1);	// the smaller the better; 0.1 would be ok
