a05db9dfdd03423471bbb8a90d6f377c262a0640
wnguyen <nguyen094@gmail.com>
2017-09-19 23:37:55 +0200

    fix typo facecheck

--- public/sqeye/js/MWDET_gazer.js
+++ public/sqeye/js/MWDET_gazer.js
@@ -176,8 +176,8 @@ window.Gazer = window.Gazer || (function () {
     closeFacecheckAndRefresh();
   };
 
-  module.closeFaceCheckContinue = function() {
-    closeFaceCheckContinue();
+  module.closeFacecheckContinue = function() {
+    closeFacecheckContinue();
   };
 
   module.startWebgazer = function () {
