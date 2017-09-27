4634183146ab4dfa2cf5fc5707d88c2dcb41e209
codesalad <nguyen094@gmail.com>
2017-09-27 17:03:56 +0200

    test changes status to play after rewind

--- public/ieye/js/iew-vcontrol.js
+++ public/ieye/js/iew-vcontrol.js
@@ -210,7 +210,6 @@ window.vcontrol = window.vcontrol || (function() {
     };
 
     module.rewindAndPlay = function(secToRewind) {
-        getCurrentPlayer().pause();
         console.log(">>>REWIND(): CURRENT VIDEO STATE = " + currentPlayerState);
 
         if (currentPlayerState === 'play') {
@@ -230,6 +229,7 @@ window.vcontrol = window.vcontrol || (function() {
             console.log('DUR= ' + totalDuration + ' Played= ' + secondsPlayed + 'FRewind to ' + restartVideoAt);
             getCurrentPlayer().seekTo(restartVideoAt); // had true as 2nd arg but seekTo here takes only 1
             getCurrentPlayer().play();
+            currentPlayerState == 'play';
         }
     };
 
