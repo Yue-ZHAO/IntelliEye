9b919929bb45d7d23f6a153d67e1b0a66cfe959c
Yue-ZHAO <zhaoyue.89@gmail.com>
2017-09-27 17:21:03 +0200

    Fix currentPlayerState == 'play';

--- public/ieye/js/iew-vcontrol.js
+++ public/ieye/js/iew-vcontrol.js
@@ -231,7 +231,7 @@ window.vcontrol = window.vcontrol || (function() {
             console.log('DUR= ' + totalDuration + ' Played= ' + secondsPlayed + 'FRewind to ' + restartVideoAt);
             getCurrentPlayer().seekTo(restartVideoAt); // had true as 2nd arg but seekTo here takes only 1
             getCurrentPlayer().play();
-            currentPlayerState == 'play';
+            currentPlayerState = 'play';
         }
     };
 
